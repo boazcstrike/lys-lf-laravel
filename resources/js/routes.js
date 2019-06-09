c6c4fef5ccdb754c559e6869b56dea5fd8c21b7c
Boaz Sze <boaz.sze@gmail.com>
2019-06-09 16:07:23 +0800

    Created navbar

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,12 +2,16 @@ import Landing from './views/Landing'
 import Articles from './views/Articles'
 import Contact from './views/Contact'
 import Profile from './views/Profile'
+import Team from './views/Team'
+import Practice from './views/Practice'
 
 const routes = [
     { path: '/', component: Landing },
     { path: '/articles', component: Articles },
     { path: '/contact', component: Contact },
-    { path: '/profile', component: Profile }
+    { path: '/profile', component: Profile },
+    { path: '/team', component: Team },
+    { path: '/practice', component: Practice },
 ]
 
 export default routes
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,14 +2,19 @@
 
 @section('content')
 
-<div id="app">
-    <nav class="navbar navbar-top">
-        <router-link to="/" class="navbar-brand" href="#">
-            <img class="logo" src="{{ asset('images/lys-logo.png') }}">
-        </a>
-        <router-link to="/contact">Contact</router-link>
-        <router-link to="/profile">Profile</router-link>
-        <router-link to="/articles">Articles</router-link>
+<div class="landing" id="app">
+    <nav class="lys-nav">
+        <div class="container">
+            <router-link class="nav-links" to="/">
+                <img class="logo" src="{{ asset('images/lys-logo.png') }}">
+            </router-link>
+            
+            <router-link class="nav-links" to="/practice">Practice Areas</router-link>
+            <router-link class="nav-links" to="/profile">Profile</router-link>
+            <router-link class="nav-links" to="/articles">News</router-link>
+            <router-link class="nav-links" to="/team">Our Team</router-link>
+            <router-link class="nav-links" to="/contact">Contact</router-link>
+        </div>
     </nav>
     <router-view></router-view>
 </div>
